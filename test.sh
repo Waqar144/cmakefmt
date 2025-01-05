cd20f3e2ec153581e1742ab0d4ff74f39102f190
Waqar Ahmed <waqar.17a@gmail.com>
2025-01-05 20:40:52 +0500

    Add basic formatting test

---
+++ test.sh
@@ -0,0 +1,11 @@
+# run unit test
+zig build test
+
+./zig-out/bin/cmakeformat test/test.cmake &> test/test.actual.cmake
+if cmp -s test/test.expected.cmake test/test.actual.cmake; then
+    echo "test passed";
+    rm test/test.actual.cmake
+else
+    echo "test failed";
+    git diff --no-index test/test.expected.cmake test/test.actual.cmake
+fi
---
+++ test/test.expected.cmake
@@ -0,0 +1,14 @@
+option(IS_TAGGED_RELEASE_BUILD
+    "Clears the \"Prelease Version\" project description for tagged release builds. Defaults to `false`" OFF
+)
+
+cmake_policy(SET CMP0012 NEW) # needed for require flags down below
+if(IUI_ENABLE_BUILD_3RDPARTY)
+    set(Runtime_data_BUILD_QT6
+        ${IUI_INSTRUMENT_QT6}
+        CACHE BOOL "Build against qt6" FORCE
+    )
+endif()
+message(STATUS
+    "CMake version: ${CMAKE_VERSION}sssssssssssssss dfaskldfj askdfj slkdfj "
+)
