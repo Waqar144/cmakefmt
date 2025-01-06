9666223dcd0472b047dbb44ed2531f5b57e695fe
Waqar Ahmed <waqar.17a@gmail.com>
2025-01-06 19:17:32 +0500

    Rename to cmakefmt

--- build.zig
+++ build.zig
@@ -16,7 +16,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "cmakeformat",
+        .name = "cmakefmt",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-./zig-out/bin/cmakeformat test/test.cmake &> test/test.actual.cmake
+./zig-out/bin/cmakefmt test/test.cmake &> test/test.actual.cmake
 if cmp -s test/test.expected.cmake test/test.actual.cmake; then
     echo "test passed";
     rm test/test.actual.cmake
